616d5ad09ef41b79177a2c85fbe6a0a76f279b9b
Aokijikuzan <florenzabs@hotmail.fr>
2021-11-05 22:44:59 +0100

    exercices pour la partie front de la formation sur la realisation d'appli web java

---
+++ src/com/directmedia/onlinestore/frontoffice/controller/HomeServlet.java
@@ -0,0 +1,50 @@
+package com.directmedia.onlinestore.frontoffice.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+@WebServlet(name="HomeServlet",urlPatterns= {"/home"})
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");//pour le type du contenue 
+        response.setCharacterEncoding("UTF-8");
+        
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset=\"utf-8\" />");
+        out.println("<title>OnlineStore</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>OnlineStore, votre boutique multimédia en ligne!!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+	}
+
+
+
+}
